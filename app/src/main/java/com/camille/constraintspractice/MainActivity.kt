00c15fc4ff95d25b0b3906a44917eed7cd7020e8
Camille <xdghost.kamil@gmail.com>
2021-06-08 18:19:58 +0300

    Some design for the Apple Music on Android

---
+++ app/src/main/java/com/camille/constraintspractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.camille.constraintspractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_main)
+    }
+}
